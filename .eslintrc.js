c1dc4b4338267597a84063dbda6e15fed2a66644
bung87 <crc32@qq.com>
2021-07-18 22:24:13 +0800

    o

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,16 @@
 module.exports = {
     "extends": [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier/@typescript-eslint"
+        "plugin:@typescript-eslint/recommended"
     ],
     "root": true,
     "parser": "@typescript-eslint/parser",
     "plugins": [
         "@typescript-eslint"
-    ]
+    ],
+    "rules":{
+        "@typescript-eslint/explicit-module-boundary-types":"off",
+        "@typescript-eslint/no-empty-function":"off"
+    }
+
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,36 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { findDependents } from "@ad2302/why-this-file";
+import path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
-
-
-	let disposable = vscode.commands.registerCommand('vscode-why-this-file.why', () => {
-		if (!vscode.window.activeTextEditor) {
-			return; // no editor
-		  }
-		  let { document } = vscode.window.activeTextEditor;
-    if (document.uri.scheme !== myScheme) {
-      return; // not my scheme
+  const disposable = vscode.commands.registerCommand(
+    "vscode-why-this-file.why",
+    () => {
+      if (!vscode.window.activeTextEditor) {
+        return; // no editor
+      }
+      const { document } = vscode.window.activeTextEditor;
+      const filePath = document.uri.path;
+      const root =  vscode.workspace.getWorkspaceFolder(document.uri);
+      console.log("filePath:",filePath)
+      console.log("root:",root?.uri.fsPath)
+      const dependents = findDependents(filePath,root?.uri.fsPath);
+      console.log("dependents:",dependents)
+      const p = vscode.window.showQuickPick(dependents, {
+        placeHolder: 'Select File',
+        matchOnDetail: true,
+      });
+      p.then((value) => {
+        if (!value) return;
+        const fn = vscode.Uri.file(path.join(root?.uri.fsPath, value))
+        vscode.workspace.openTextDocument(fn).then((doc) => {
+          return vscode.window.showTextDocument(doc);
+        });
+      });
     }
-	const filePath = document.uri.path
-	});
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
